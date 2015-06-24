62ae3136ae87d2fde5442d261386887619eb5f1f
gvacaliuc <gabe.vacaliuc@gmail.com>
2015-06-24 13:30:33 -0400

    shielded universe load

--- dQAA.py
+++ dQAA.py
@@ -18,11 +18,11 @@ def mmap_concat(a,b):
 	return c
 
 def phisel(res):
-#MDAnalysis' phi_selection requires a segid be present, this doesn't.  use if MDanalysis fails.
+#MDAnalysis' phi_selection requires a segid be present while this doesn't.
 	return res.universe.selectAtoms('resid %d and name C' %(res.id-1) ) + res.N + res.CA + res.C
 
 def psisel(res):
-#MDAnalysis' phi_selection requires a segid be present, this doesn't.  use if MDanalysis fails.
+#MDAnalysis' phi_selection requires a segid be present while this doesn't. 
 	return res.N + res.CA + res.C + res.universe.selectAtoms('resid %d and name N' %(res.id+1) )
 
 
@@ -30,7 +30,7 @@ def psisel(res):
 num_traj = 1
 rad_gyr = []
 
-def main(num_traj, ica_dim):
+def qaa(num_traj, ica_dim):
 	for i in range(num_traj):
 		
 		#	!Edit to your trajectory format!
@@ -66,7 +66,7 @@ def main(num_traj, ica_dim):
 		phidat.compute(u.trajectory)
 		psidat.compute(u.trajectory)
 	
-		#	Converts to nd-array and changes from [69,1,10K] to [69,10K]
+		#	Converts to nd-array and changes from [numRes,1,numSamples] to [numRes,numSamples]
 		phidat =  array(phidat)
 		phidat = phidat.reshape(phidat.shape[0],phidat.shape[2])
 		psidat =  array(psidat)
@@ -86,106 +86,6 @@ def main(num_traj, ica_dim):
 			fulldat = mmap_concat(fulldat, dihedral_dat);
 		for ts in u.trajectory:
 			rad_gyr.append( atom.radiusOfGyration() )
-	
-	#	Original AnharmonicHisto code showing kurtosis, pca plots, etc...
-	"""
-	print array(tmp).shape
-	itr = []; avgCoords = []; eRMSD = []; newCoords = [];
-	
-	for i in range(num_coords):
-		[a, b, c, d] = iterAlign.iterativeMeans(array(tmp)[i,:,:,:], 0.001, 4);
-		itr.append(a);
-		avgCoords.append(b[-1]);
-		eRMSD.append(c);
-		newCoords.append(d);
-	
-	print array(avgCoords).shape
-	
-	#Final averaging
-	[itr, avgCoords, eRMSD, newCoords] = iterAlign.iterativeMeans(array(avgCoords), 0.001, 4);
-	
-	print 'eRMSD shape: ', numpy.shape(eRMSD);
-	print 'newC shape: ', newCoords.shape;
-	print 'len: ', len(newCoords)
-	coords = numpy.reshape(newCoords, (len(newCoords), dim*Na)).T;
-	
-	print 'coords: ', numpy.shape(coords); 
-	
-	avgCoords = numpy.mean(coords, 1); print avgCoords;
-	
-	print 'avgCoords: ', numpy.shape(avgCoords);
-	
-	tmp = numpy.reshape(numpy.tile(avgCoords, num_coords), (num_coords,dim*Na)).T;
-	caDevsMD = coords - tmp;
-	#print numpy.shape(caDevsMD); print caDevsMD[0];
-	D = caDevsMD.flatten(); print numpy.shape(D);
-	gm = numpy.mean(D); 
-	gs = numpy.std(D);
-	gK = scipy.stats.kurtosis(D,0,fisher=False);
-	
-	[n,s] = numpy.histogram(D, bins=51,normed=1);
-	
-	gp = numpy.exp(-(s-gm)**2/(2*gs*gs));
-	gp = gp/numpy.sum(gp); print numpy.shape(gp);
-	
-	lo = 0; hi = len(s);
-	
-	fig = plt.figure();
-	ax = fig.add_subplot(111);
-	x = 0.5*(s[1:] + s[:-1]);
-	#ax.semilogy(s[lo:hi], gp[lo:hi],'c-',linewidth=2);
-	ax.hold(True); 
-	ax.semilogy(x, n, 'k-', linewidth=2.0); ax.axis('tight');
-	print 'fig1'
-	plt.show();
-	
-	
-	#print 'Mean: ', gm;
-	print 'Std. dev: ', gs;
-	print 'Kurtosis: ', gK;
-	
-	cc = coords[:,0]; print numpy.shape(cc);
-	cc = numpy.reshape(cc, (dim,Na));
-	#print numpy.shape(coords[0:-1:3,0]), numpy.shape(coords[1:-1:3,0]), numpy.shape(coords[2:-1:3,0]);
-	
-	fig = plt.figure();
-	ax = fig.add_subplot(111, projection='3d');
-	ax.plot(cc[0,:], cc[1,:], cc[2,:]);
-	print 'fig2'
-	plt.show();
-	
-	print numpy.shape(numpy.cov(coords));
-	numpy.save('cov_ca.npy', numpy.cov(coords));
-	[pcas,pcab] = numpy.linalg.eigh(numpy.cov(coords));
-	#numpy.save('pcab_ca.npy', pcab);
-	print pcas.shape, pcab.shape
-	print 'pcas: ', pcas
-	si = numpy.argsort(-pcas.ravel()); print si;
-	pcaTmp = pcas;
-	pcas = numpy.diag(pcas);
-	pcab = pcab[:,si];
-
-	#fig = plt.figure();
-	#ax = fig.add_subplot(111);
-	#cs = ax.contourf(numpy.cov(coords));
-	##ax.colorbar(cs); 
-	#plt.show();
-	
-	fig = plt.figure();
-	ax = fig.add_subplot(111);
-	y = numpy.cumsum(pcaTmp.ravel()/numpy.sum(pcaTmp.ravel()));
-	ax.plot(y);
-	print 'fig3';
-	plt.show();
-	
-	fig = plt.figure();
-	ax = fig.add_subplot(111, projection='3d');
-	pcacoffs = numpy.dot(pcab.conj().T, caDevsMD);
-	print numpy.shape(pcacoffs);
-	ax.scatter(pcacoffs[0,:], pcacoffs[1,:], pcacoffs[2,:], marker='o', c=[0.6,0.6,0.6]);
-	print 'fig4';
-	plt.show();
-	"""
 	
 	#	some set up for running JADE
 	print 'fulldat: ', fulldat.shape
@@ -212,4 +112,4 @@ def main(num_traj, ica_dim):
 	plt.show();
 
 if __name__ == '__main__':
-	main(1, 40);
+	qaa(1, 40);
