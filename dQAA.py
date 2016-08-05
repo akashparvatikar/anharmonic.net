c6d0915a4c1b7dc66bd2dfff0e575ddf12d04723
gvacaliuc <gabe.vacaliuc@gmail.com>
2016-08-05 15:17:08 -0400

    Updated dQAA to correct format

--- dQAA.py
+++ dQAA.py
@@ -1,27 +1,47 @@
 import numpy
 import numpy as np
-from numpy import *
 import math
 import scipy.stats
+from scipy.stats import kurtosis
+from numpy import *
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from MDAnalysis.core.Timeseries import *
-from MDAnalysis import *
-import MDAnalysis
-from jade import *
+ 
 import argparse
-import warnings
-import timing
-import os.path as path
+import os
+import logging
+import pickle
 
+from KabschAlign import *
+from IterativeMeansAlign import *
+from jade import *
+
+from MDAnalysis import Universe
+import MDAnalysis.version as v
+
+
+mdversion = v.__version__;
+tmp = '';
+b = False;
+for i in mdversion:
+    if i == '.': b = not(b);
+    if b: tmp += i;
+tmp = tmp.strip('.');
+assert( int(tmp) >= 11 );
+
+log = logging.getLogger('main.cQAA');
+log.setLevel(logging.DEBUG);
+
+#================================================
 #a is mem-mapped array, b is array in RAM we are adding to a.
 def mmap_concat(shape,b,filename):
-	assert(shape[0] == b.shape[0]);
-	c = np.memmap(filename, dtype='float64', mode='r+', shape=(shape[0],shape[1]+b.shape[1]))
-	c[:, shape[1]:] = b
-	newshape = c.shape;	
-	del c;	#	Flushes changes to memory and then deletes.  New array can just be called using filename
-	return newshape;
+    assert(shape[1] == b.shape[1] and shape[2] == b.shape[2]);
+    c = np.memmap(filename, dtype='float64', mode='r+', shape=(shape[0]+b.shape[0],shape[1],shape[2]))
+    c[shape[0]:, :, : ] = b
+    newshape = c.shape;    
+    del c;    #    Flushes changes to memory and then deletes.  New array can just be called using filename
+    return newshape;
+#================================================
 
 def phisel(res, resid):
 #MDAnalysis' phi_selection requires a segid be present while this doesn't.
@@ -31,63 +51,80 @@ def psisel(res, resid):
 #MDAnalysis' phi_selection requires a segid be present while this doesn't. 
 	return res[resid].N + res[resid].CA + res[resid].C + res[resid+1].N
 
-def qaa(config, val):
-	num_traj = config['numOfTraj'];
-	ica_dim = config['icadim'];
-	start_traj = config['startTraj'];
-	filename = path.join('./.memmapped','dihedral_data.array');
+def dqaa(config, val):
+
+    #    Pull config from config
+    startRes = config['startRes'];
+    endRes = config['endRes']+1;
+    sliceVal = config['sliceVal'];
+    trajectories = config['trajectories'];
+    savedir = config['saveDir'];
+    figdir = config['figDir'];
+
+    filename = os.path.join(savedir,'.memmapped','coord_data.array');
 
 	#	Loops through trajectories --------------------------------------------
-	for i in range(start_traj,num_traj+start_traj):
+    count = 0;
+	for traj in trajectories:
+        count+=1;
 		#	!Edit to your trajectory format!
 		try:
-			u = MDAnalysis.Universe("pentapeptide/init-ww-penta.pdb", "pentapeptide/job%i-protein.dcd" %(i), permissive=False);
+			pdb = config['pdbfile'];
+            dcd = traj;
+            u = Universe(pdb, dcd, permissive=False);
 		except:
-			raise ImportError('You must edit \'dQAA.py\' to fit your trajectory format!');
-			exit();
-	
-		atom = u.selectAtoms('backbone');
+            log.debug('PDB: {0}\nDCD: {1}'.format(pdb, dcd));
+            raise ImportError('You must edit \'config.yaml\' to fit your trajectory format!');
+
+        protein_residues = u.n_residues;
+        
 	
 		phidat = TimeseriesCollection()
 		psidat = TimeseriesCollection()
 
 		#	Adds each (wanted) residues phi/psi angles to their respective timeseries collections
-		if val.verbose: timing.log('Processing Trajectory %i' %(i+1));
-		numres = config['numRes']
-		trajlen = len(u.trajectory)
-		tmplen = trajlen/config['slice_val'];
+		log.info('Processing Trajectory {0}...'.format(count));
 
-		#	Tailor generateConfig.py for correct for-loop iterations
-		for res in range(1+config['startRes'], config['startRes']+numres):
+        start   = np.max( [startRes, 1] );
+        end     = np.min( [endRes, protein_residues - 1] );
+
+        resnames = u.atoms.CA.resnames[start:end];        
+
+        numRes  = end - start + 1;
+		for res in range(start, end):
 			#	selection of the atoms involved for the phi angle
 			phi_sel = phisel(u.residues,res);
 			#	selection of the atoms involved for the psi angle
-			psi_sel = psisel(u.residues,res-1);
+			psi_sel = psisel(u.residues,res);
 
 			phidat.addTimeseries(Timeseries.Dihedral(phi_sel))
 			psidat.addTimeseries(Timeseries.Dihedral(psi_sel))
 
-		useable_res = numres-1;	#	Phi/Psi require a buffer residue to compute
 		#	Computes along whole trajectory
-		phidat.compute(u.trajectory, skip=config['slice_val'])
-		psidat.compute(u.trajectory, skip=config['slice_val'])
+		phidat.compute(u.trajectory, skip=config['sliceVal'])
+		psidat.compute(u.trajectory, skip=config['sliceVal'])
 	
-		#	Converts to nd-array and changes from [numRes,1,numSamples] to [numRes,numSamples]
-		phidat =  array(phidat).reshape(useable_res,tmplen);
-		psidat =  array(psidat).reshape(useable_res,tmplen);
+		#	Converts to nd-array and changes from [numRes,1,sliced numSamples] 
+        #   to [numRes,sliced numSamples]
+		phidat =  array(phidat).reshape(numRes,-1);
+		psidat =  array(psidat).reshape(numRes,-1);
 		
 		"""	Data stored as  | sin( phi_(i) )   |---
 							| cos( phi_(i) )   |---
-							| sin( psi_(i-1) ) |---
-							| cos( psi_(i-1) ) |---"""
-		didat = np.zeros((4*phidat.shape[0], phidat.shape[1]));
+							| sin( psi_(i) ) |---
+							| cos( psi_(i) ) |---"""
+		didat = np.empty((4*phidat.shape[0], phidat.shape[1]));
 		didat[0::4,:] = np.sin(phidat);
 		didat[1::4,:] = np.cos(phidat);
 		didat[2::4,:] = np.sin(psidat);
 		didat[3::4,:] = np.cos(psidat);
 
-		if i == start_traj:
-			fulldat = np.memmap(filename, dtype='float64', mode='w+', shape=(useable_res*4, tmplen))
+		if count == 1:
+			fulldat = np.memmap(filename,
+                                dtype='float64',
+                                mode='w+',
+                                shape=(useable_res*4, tmplen),);
+
 			fulldat[:,:] = didat.astype('float64');
 			map_shape = fulldat.shape;
 			del fulldat;
@@ -97,105 +134,191 @@ def qaa(config, val):
 	#	End loop --------------------------------------------------------------
 	
 	fulldat = np.memmap(filename, dtype='float64', mode='r+', shape=map_shape);
-	#	determining % time exhibiting anharmonicity ---------------------------
-	anharm = np.zeros((2,numres));
-	for i in range(2):
-		for j in range(numres):
-			tmp = fulldat[i::2,:][j]
-			median = np.median(tmp);
-			stddev = np.std(tmp);
-			anharm[i,j] = float( np.sum( (np.abs(tmp - median) > 2*stddev) ) ) / fulldat.shape[1];
-
-	if val.save: np.save('savefiles/dih_anharm_%s.npy' %(config['pname']), anharm );
-	#	END ANHARM ------------------------------------------------------------
-
-	if val.save and __name__ == '__main__': np.save('savefiles/%s_dihedraldata.npy' %(config['pname']), fulldat[:,:]);
-	del fulldat;
 	
-	icajade, icafile, mapshape = jade_calc(config, filename, map_shape, val);
+    np.save(os.path.join(savedir, '{0}_dihedral_data.npy'.format(config['pname'])), fulldat[:,:]);
+    np.save(os.path.join(savedir, '{0}_resnames.npy'.format(config['pname'])), resnames);
+    
+    del fulldat;
+
+	icajade, icafile, mapshape = jade_calc(config, filename, map_shape);
 	return icajade, icafile, mapshape;
+#================================================
 
-#==============================================================================
-def jade_calc(config, filename, mapshape, val):
-	
-	numsamp = mapshape[1];
-	coords = np.memmap(filename, dtype='float64', mode='r+', shape=mapshape);	
-	if val.debug:	
-		avgCoords = numpy.mean(coords, 1); 
-		print avgCoords;
-		print 'avgCoords: ', numpy.shape(avgCoords);
+#================================================
+def genCumSum(config, pcas, pcab):
+    savedir = config['saveDir'];
+    figdir = config['figDir'];
 
-	#==========================================================================	
-	#	Setup to determine proper ICA dimensionality
-	if val.setup:
-		[pcas,pcab] = numpy.linalg.eig(numpy.cov(coords));
-		si = numpy.argsort(-pcas.ravel());
-		pcaTmp = pcas;
-		pcas = numpy.diag(pcas);
-		pcab = pcab[:,si];
-		
-		fig = plt.figure();
-		ax = fig.add_subplot(111);
-		y = numpy.cumsum(pcaTmp.ravel()/numpy.sum(pcaTmp.ravel()));
-		ax.plot(y*100);
-		ax.set_xlabel('Number of Principal Components');
-		ax.set_ylabel('Percent of Total Variance Preserved');
-		ax.set_title('Variance of Principal Components');
-		if val.verbose: print('Cov. Matrix spectrum cum. sum.');
-		plt.show();
-		a = input('Enter desired ICA dimension (enter -1 for default): ');
-		if (a > 0):
-			config['icadim'] = a;
-	#	End ICA Setup
-	#==========================================================================
+    si = numpy.argsort(-pcas.ravel());
+    pcaTmp = pcas;
+    pcas = numpy.diag(pcas);
+    pcab = pcab[:,si];
+    
+    fig = plt.figure();
+    ax = fig.add_subplot(111);
+    y = numpy.cumsum(pcaTmp.ravel()/numpy.sum(pcaTmp.ravel()));
+    ax.plot(y*100);
+    ax.set_xlabel('Number of Principal Components');
+    ax.set_ylabel('Percent of Total Variance Preserved');
+    ax.set_title('Variance of Principal Components');
 
-	# some set up for running JADE
-	subspace = config['icadim']; # number of IC's to find (dimension of PCA subspace)
-	
-	#	Performs jade and saves if main
-	coords.flush();
-	icajade = jadeR(filename, mapshape, val, subspace);
-	if __name__ == '__main__' and (val.save): np.save('savefiles/icajade_%s_%i.npy' %(config['pname'], config['icadim']), icajade) 
+    pickle.dump(fig, file(os.path.join(figdir, '{0}_cumsum.pickle'.format(config['pname'])), 'w') );
+    plt.savefig(os.path.join(figdir, '{0}_cumsum.png'.format(config['pname'])));
 
-	if val.debug: print 'icajade: ', numpy.shape(icajade);
+    if 'setup' in config and config['setup']:
+        log.info('Cov. Matrix spectrum cum. sum:');
+        plt.show();
+        a = input('Enter desired ICA dimension (enter -1 for default): ');
+        if (a > 0):
+            config['icaDim'] = a;
+#================================================
 
-	#	Performs change of basis
-	icafile = path.join('./.memmapped','icacoffs.array');
-	icacoffs = np.memmap(icafile, dtype='float64', mode='w+', shape=(config['icadim'],numsamp) );
-	icacoffs[:,:] = icajade.dot(coords)
-	icacoffs.flush();
-	if val.debug: print 'icacoffs: ', numpy.shape(icacoffs);
-	if (val.save) and __name__ == '__main__': np.save('savefiles/icacoffs_%s_%i.npy' %(config['pname'], config['icadim']), icacoffs[:,:]) 
-	
-	if val.graph:	
-		fig = plt.figure();
-		ax = fig.add_subplot(111, projection='3d');
-		ax.scatter(icacoffs[0,::10], icacoffs[1,::10], icacoffs[2,::10], marker='o', c=[0.6,0.6,0.6]); 
-		print 'First 3 Dimensions of Icacoffs';
-		plt.show();
+#================================================
+def genKurtosisPlot(config, filename, mapshape):
+    mapcoords = np.memmap(filename, 'r', dtype='float64', shape=mapshape);
 
-	#	Returns icajade matrix, ica filename, and the shape.
-	return icajade, icafile, icacoffs.shape;
+    #   May have to truncate
+    numTraj = mapcoords.shape[1] // config['trajLen'];
+    plotindv = numTraj <= 5;
+    mapcoords = mapcoords[:,:numTraj*config['trajLen']];
+
+    mapcoords[:,:] = mapcoords - np.mean(mapcoords, axis=1).reshape((-1,1));
+    
+    #   Using Pearson's defn of kurtosis: (Fisher + 3)
+    d = mapcoords.flatten();
+    mean    = np.mean(d);
+    stddev  = np.std(d);
+    kurt    = kurtosis(d, 0, fisher=0);
+
+    values, edges = np.histogram(d, bins=51, normed=1);
+    gauss_curve = np.exp( - ((edges - mean)**2.)/(2*stddev**2.));
+    #   Normalizes such that the integral of gauss_curve over boundary is 1
+    gauss_curve = gauss_curve / ( np.sum(gauss_curve) * np.abs(edges[1]-edges[0]) );
+    midpts = .5*( edges[1:] + edges[:-1] );
+
+    fig = plt.figure();
+    ax = fig.gca();
+    #   Log-Plot
+    ax.semilogy(midpts, values, 'r-', label='{0} Data, Kurtosis: {1}'.format(config['pname'], kurt));
+    #   gauss curve not very useful
+    #ax.semilogy(edges, gauss_curve, 'b-', label='Gaussian Curve, Kurtosis: 3');
+
+    if plotindv:
+        kurtdata = [];
+        scale = mapcoords.shape[0]*config['trajLen'];
+        for i in range(numTraj):
+            kurtdata.append(kurtosis(d[i*scale:(i+1)*scale], 0, fisher=False));
+
+        val, edg, mid = [], [], [];
+        for i in range(numTraj):
+            a, b = np.histogram(d[i*scale:(i+1)*scale], bins=51, normed=1);
+            mpt = (b[1:]+b[:-1])/2.;
+            val.append(a);
+            edg.append(b);
+            mid.append(mpt);
+
+        for i in range(numTraj):
+            ax.semilogy( mid[i], val[i], label='{0} Data, Traj. {1},  Kurtosis: {2}'.format(config['pname'], i+1, kurtdata[i]));
+
+    box = ax.get_position();
+    ax.set_position([box.x0, box.y0+box.height*.2, box.width, box.height*.8]);
+    ax.legend(loc='upper center', bbox_to_anchor=(.5, -.1), fancybox=True, fontsize=8);
+    ax.set_title('Plotted Histogram of {0} data (Log y-scale)'.format(config['pname']));
+
+    plt.savefig( os.path.join(config['figDir'], '{0}_loghistogram.png'.format(config['pname'])) );
+    pickle.dump( fig, file( os.path.join(config['figDir'], '{0}_loghistogram.pickle'.format(config['pname'])), 'w+') );
+
+    if 'graph' in config and config['graph']:
+        plt.show();
+
+    plt.close();
 
 
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-g', action='store_true', dest='graph', default=False, help='Shows graphs.')
-	parser.add_argument('-v', action='store_true', dest='verbose', default=False, help='Runs program verbosely.')
-	parser.add_argument('-s', '--save', action='store_true', dest='save', default=False, help='Saves important matrices.')
-	parser.add_argument('-d', '--debug', action='store_true', dest='debug', default=False, help='Prints debugging help.')
-	parser.add_argument('--setup', action='store_true', dest='setup', default=False, help='Runs setup calculations: Cum. Sum. of cov. spectrum\nand unit radius neighbor search.')
-	parser.add_argument('--single', action='store_true', dest='single', default=False, help='Runs jade w/ single precision. NOT recommended.')
-	parser.add_argument('--smart', action='store_true', dest='smart', default=False, help='Runs jade using an alternative diagonalization setup. Refer to Cardoso\'s code for more details.')
+#================================================
 
-	values = parser.parse_args()
-	if values.debug: values.verbose = True;
+#================================================
+def jade_calc(config, filename, mapshape):
+    dim = 3;
+    numres = mapshape[0]/dim;
+    numsamp = mapshape[1];
+    coords = np.memmap(filename, dtype='float64', shape=mapshape);
+    savedir = config['saveDir'];
+    figdir = config['figDir'];
 
-#	Config settings -- only here if dQAA is called directly from python
-	config = {};
-	config['numOfTraj'] = 1;
-	config['icadim'] = 40;
-	config['pname'] = 'PROTEIN';	#	Edit to fit your protein name
-	config['startRes'] = 0;			# 	excludes first
-	config['endRes'] = -2;			#	excludes last
-	qaa(config, values);
+    #    Plots (if `setup`) Cum. Sum of Variance in PC's
+    [pcas,pcab] = numpy.linalg.eig(numpy.cov(coords));
+    genCumSum(config, pcas, pcab);
+
+    #   Plots Histogram
+    genKurtosisPlot( config, filename, mapshape );
+
+    # some set up for running JADE
+    subspace = config['icaDim']; # number of IC's to find (dimension of PCA subspace)
+    
+    #    Performs jade and saves if main
+    coords.flush();
+    icajade = jadeR(filename, mapshape, subspace);
+    np.save(os.path.join(savedir, '{0}_icajade_{1}dim.npy'.format(config['pname'], config['icaDim'])), icajade); 
+    log.debug('icajade: {0}'.format(numpy.shape(icajade)));
+
+    #    Performs change of basis
+    icafile = os.path.join(savedir,'.memmapped','icacoffs.array');
+    icacoffs = np.memmap(icafile, dtype='float64', mode='w+', shape=(config['icaDim'],numsamp) );
+    icacoffs[:,:] = icajade.dot(coords)
+    icacoffs.flush();
+
+    #   Pulls it out of the hidden directory for later use
+    public_icafile = os.path.join(savedir, '{0}_icacoffs_{1}dim.array'.format(config['pname'],config['icaDim']));
+    publicica = np.memmap(public_icafile, dtype='float64', mode='w+', shape=(config['icaDim'],numsamp) );
+    publicica[:,:] = icacoffs[:,:];
+    publicica.flush();    
+    del publicica;
+
+    log.debug('icacoffs: {0}'.format(numpy.shape(icacoffs)));
+    np.save(os.path.join(savedir, '{0}_icacoffs_{1}dim.npy'.format(config['pname'], config['icaDim'])), icacoffs[:,:]) 
+
+    fig = plt.figure();
+    ax = fig.add_subplot(111, projection='3d');
+    ax.scatter(icacoffs[0,::10], icacoffs[1,::10], icacoffs[2,::10], marker='o', c=[0.6,0.6,0.6]);
+
+    #    Saves figure object to pickle, saves figure plot to png
+    pickle.dump(fig, file(os.path.join(figdir, '{0}_icacoffs_scatter.pickle'.format(config['pname'])), 'w') );
+    plt.savefig(os.path.join(figdir, '{0}_icacoffs_scatter.png'.format(config['pname'])));
+    
+    #    If in CLI and graph flag
+    if 'graph' in config and config['graph']:
+        log.info('First 3 Dimensions of Icacoffs');
+        plt.show();
+
+    #    Returns icajade matrix, ica filename, and the shape.
+    return icajade, icafile, icacoffs.shape;
+#================================================
+
+#================================================
+"""
+    INPUT:
+    fulldat     = atom coordinates      -- size [NumAtoms, 3, NumRes];
+    atomname    = list of atom names    -- length NumRes
+    resname     = list of residuee names-- length NumRes
+    filename    = filename to save to: e.g. 'filename.pdb'
+    verbose     = boolean to  
+"""
+
+def pdbgen(fulldat, atomname, resname, filename, verbose):
+    
+    numatom, dim, numres = fulldat.shape;
+    assert ( len(atomname) == numres );
+    assert ( len(resname) == numres );
+
+    if verbose: print 'Constructing PDB file...';
+    f = open('savefiles/%s.pdb' %(filename), 'w+');
+    for i in range(fulldat.shape[0]):
+        f.write('%-6s    %4i\n' %('MODEL', i+1));
+        for j in range(fulldat.shape[2]):
+            f.write('%-6s%5i %4s %s  %4i    %8.3f%8.3f%8.3f%6.2f%6.2f           C  \n' \
+                %('ATOM', j+1, atomname[j], resname[j], j+1, fulldat[i,0,j], fulldat[i,1,j], fulldat[i,2,j], 0.0, 0.0,));
+        f.write('ENDMDL\n');
+    f.close();
+    if verbose: print 'PDB file completed!';
+
+#================================================
