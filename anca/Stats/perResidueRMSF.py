8b85bd7f877fd0ad9a62a633b00b892b567ca254
acadev <acadeveloper0@gmail.com>
2017-06-26 13:23:37 -0400

    Update perResidueRMSF.py

--- anca/Stats/perResidueRMSF.py
+++ anca/Stats/perResidueRMSF.py
@@ -1,10 +1,7 @@
 import numpy
 
 def perResidueRMSF(caDevsMDall, dim=None, Na=None):
-
-xx1 = np.std(caDevsMDall, 1);
-rmsf_1 = np.mean(xx1.reshape((dim, Na)),0);
-plt.plot(rmsf_1, 'k-');
-plt.xlabel('Residue number', fontsize=20);
-plt.ylabel('RMSF (\AA)', fontsize=20);
-plt.show();
+  xx1 = numpy.std(caDevsMDall, 1);
+  rmsf_1 = numpy.mean(xx1.reshape((dim, Na)),0);
+  
+  return rmsf_1;
