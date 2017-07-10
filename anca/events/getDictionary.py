4e8aa82adb362ace77030add3bc1c8e58d313b16
acadev <acadeveloper0@gmail.com>
2017-07-10 13:21:51 -0400

    Create getDictionary.py

---
+++ anca/events/getDictionary.py
@@ -0,0 +1,16 @@
+def getDict(topfile, traj_list, dim, Na):
+    
+    my_dict = {}
+    last = 0
+    a = []
+    k = 1
+    a.append(0)
+    for i in range(0, size(traj_list)):
+        frames = (getCoordinates(topfile, traj_list[i]))
+        a.append(size(frames)/(dim*Na))
+        a[k] = a[k] + a[k-1]
+        lst = list(range(last, a[k]))
+        my_dict[traj_list[i]] = lst   
+        last = a[k]
+        k = k + 1  
+    return my_dict
