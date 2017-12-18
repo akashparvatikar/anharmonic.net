930858573f4f3f0bb20f0311567e84f94604ba4f
acadev <acadeveloper0@gmail.com>
2017-12-18 13:35:26 -0500

    Update getLongTails.py

--- anca/stats/getLongTails.py
+++ anca/stats/getLongTails.py
@@ -8,8 +8,10 @@ def getLongTails(caDevsMDall):
   gm = numpy.mean(D); 
   gs = numpy.std(D);
   gK = scipy.stats.kurtosis(D,0,fisher=False);
+  gK_Zscore, gK_pvalue = scipy.stats.kurtosistest(D, 0);
   
   print 'Overall kurtosis for system: ' + str(gK);
+  print 'Zscore and p-value for system: ' + str(gK_Zscore) +' , ' + str(gK_pvalue);
   gp = numpy.exp(-(s-gm)**2/(2*gs*gs));
   gp = gp/numpy.sum(gp); 
   
