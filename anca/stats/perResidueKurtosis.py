7f31c7ae38d0fe26ab5b9b3739247efdde683e53
acadev <acadeveloper0@gmail.com>
2017-12-21 23:38:23 -0500

    Update perResidueKurtosis.py

--- anca/stats/perResidueKurtosis.py
+++ anca/stats/perResidueKurtosis.py
@@ -11,13 +11,8 @@ def perResidueKurtosis(caDevsMDall, Na=None):
       kX = scipy.stats.kurtosis(caDevsMDall[i,:],0,fisher=False);
       kY = scipy.stats.kurtosis(caDevsMDall[i+1,:],0,fisher=False);
       kZ = scipy.stats.kurtosis(caDevsMDall[i+2,:],0,fisher=False);
-      kXZscore, kXpvalue = scipy.stats.kurtosistest(caDevsMDall[i,:],0);
-      kYZscore, kYpvalue = scipy.stats.kurtosistest(caDevsMDall[i+1,:],0);
-      kZZscore, kZpvalue = scipy.stats.kurtosistest(caDevsMDall[i+2,:],0);
       gK_Res[k] = numpy.mean([kX, kY, kZ],0);
-      gK_Res_Zscore[k] = np.mean([kXZscore, kYZscore, kZZscore], 0);
-      gK_Res_pvalue[k] = np.mean([kXpvalue, kYpvalue, kZpvalue], 0);
       i = i + 3;
       k = k + 1;
   
-  return gK_Res, gK_Res_Zscore, gK_Res_pvalue;
+  return gK_Res;
