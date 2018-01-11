fbbb017e9a91d4160c3b65bb3190066a745fc73e
AKASH PARVATIKAR <akp47@pitt.edu>
2018-01-11 18:36:42 -0500

    Update getInstantaneousKurtosis.py

--- anca/events/getInstantaneousKurtosis.py
+++ anca/events/getInstantaneousKurtosis.py
@@ -4,12 +4,22 @@ import scipy.stats
 def getInstantaneousKurtosis(caDevsMDall, dt, windowsize, halflife, wt, perRes=True, smooth=False):
     kresVals = numpy.zeros((caDevsMDall.shape[0], caDevsMDall.shape[1]));
     kvals = numpy.zeros((caDevsMDall.shape[1],1));
-   
+    kresZscorevals = numpy.zeros((caDevsMDall.shape[0],caDevsMDall.shape[1]));
+    krespvals = numpy.zeros((caDevsMDall.shape[0],caDevsMDall.shape[1]));
+    
+    kZscorevals =  numpy.zeros((caDevsMDall.shape[1],1));
+    kpvals = numpy.zeros((caDevsMDall.shape[1],1));
     for i in range(0, caDevsMDall.shape[1]):
         cc = caDevsMDall[:, i:i+windowsize];
         kresVals[:, i] = scipy.stats.kurtosis(cc, axis=1, fisher=False);
         kvals[i] = scipy.stats.kurtosis(cc, axis=1, fisher=False).mean();
-        
+        kresZscorevals[:,i],krespvals[:,i] = scipy.stats.kurtosistest(cc, axis=1, nan_policy='omit');
+    
+    kZscorevals = numpy.mean(kresZscorevals, axis = 0);    
+    kpvals = numpy.mean(krespvals, axis = 0);
+    kZscore = kZscorevals.tolist();
+    kpvalue = kpvals.tolist();
+    
     c = numpy.zeros(caDevsMDall.shape[1]-windowsize);
     if not perRes and smooth:
         val = numpy.zeros((windowsize,1));
@@ -19,8 +29,8 @@ def getInstantaneousKurtosis(caDevsMDall, dt, windowsize, halflife, wt, perRes=T
             b = kvals[i + windowsize:i:-1];
             b = b.reshape(windowsize);
             c[i] = numpy.dot(a,b);
-        return c, kvals;
+        return c, kvals, kZscore, kpvalue;
     elif perRes:
-        return kresVals, kvals;
+        return kresVals, kvals, kresZscorevals, krespvals;
     else:
         return kvals;
